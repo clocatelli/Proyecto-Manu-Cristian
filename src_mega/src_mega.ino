86597838a11a33dd95bf215d95c74e46168d20b8
Cristian <cristian@cristian.cristian.com>
2018-06-22 19:09:52 -0300

    [Coding] Modified source to be used with another ble module

--- src_mega/src_mega.ino
+++ src_mega/src_mega.ino
@@ -1,28 +1,32 @@
+#include <TimerOne.h>
+#define BTmaster  Serial1
+#define BTslave   Serial2
 String c;
+
+
+//void timerInterruption ();
 
 void setup() {
   // put your setup code here, to run once:
-  Serial1.begin(9600);
+  BTmaster.begin(9600);
+  BTslave.begin(9600);
   Serial.begin(9600);
-  Serial.setTimeout(20);
-    sendCommand("AT");
-//  sendCommand("AT+ROLE0");
-//  sendCommand("AT+UUID0xFFE0");
-//  sendCommand("AT+CHAR0xFFE1");
-//  sendCommand("AT+NAMEbluino");
-}
+  Serial.setTimeout(100);
 
-void sendCommand(String command){
-  Serial.print("Command send :");
-  Serial.print(command);
-  Serial1.print(command);
-  //wait some time
-  delay(100);
+//  Timer1.initialize();
+//  Timer1.start();
+//  Timer1.attachInterrupt(timerInterruption);
+
+  delay(1000);
+  BTmaster.println("AT");
+  //BTmaster.println("AT+INQ");
   
-  /*while (Serial1.available()) {
-    Serial.write(Serial1.read());
-  }
-  Serial.println("Reply end");*/
+  Serial1.available();
+
+//  delay(3000);
+//  Serial1.print("AT+CONN1");
+//  Serial1.available();
+
 }
 
 void loop() {
@@ -30,14 +34,16 @@ void loop() {
   if(Serial.available())
   {
     c = Serial.readString();
-    sendCommand(c);
+    Serial.print("Command send :");
+    Serial.print(c);
+    Serial1.print(c);
+    //wait some time
+    delay(100);
   }
 
   if(Serial1.available())
   {
     Serial.write(Serial1.read());
   }
-
-  
-
 }
+
