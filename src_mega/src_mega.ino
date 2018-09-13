3633c729f843b6a558472f77b53d6e05f39a8956
Cristian <cristian@cristian.cristian.com>
2018-09-12 23:34:13 -0300

    [Prueba] Cambios en los .ino para prueba de distancia.

--- src_mega/src_mega.ino
+++ src_mega/src_mega.ino
@@ -1,32 +1,12 @@
 #include <TimerOne.h>
 #define BTmaster  Serial1
-#define BTslave   Serial2
 String c;
 
-
-//void timerInterruption ();
-
 void setup() {
-  // put your setup code here, to run once:
+  // Setup interfaces
   BTmaster.begin(9600);
-  BTslave.begin(9600);
   Serial.begin(9600);
   Serial.setTimeout(100);
-
-//  Timer1.initialize();
-//  Timer1.start();
-//  Timer1.attachInterrupt(timerInterruption);
-
-  delay(1000);
-  BTmaster.println("AT");
-  //BTmaster.println("AT+INQ");
-  
-  Serial1.available();
-
-//  delay(3000);
-//  Serial1.print("AT+CONN1");
-//  Serial1.available();
-
 }
 
 void loop() {
---
+++ src_nano/src_nano.ino
@@ -0,0 +1,70 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTslave(3, 2); // RX, TX
+const int openPin = 7;   // Pin to open shutter.
+const int closePin = 8;  // Pin to close shutter.
+const int stopPin = 9;   // Pin to stop shutter movement.
+
+const int openEnd = 4;   // Pin to open shutter.
+const int closeEnd = 5;  // Pin to close shutter.
+const int stopEnd = 6;   // Pin to stop shutter movement.
+
+char moveDir = 's';      // Set default state of move direction.
+
+void setup() 
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+
+  // Set pins to input signals.
+  pinMode(openEnd, INPUT);
+  pinMode(closeEnd, INPUT);
+  pinMode(stopEnd, INPUT);
+
+  // Set pins to output signals.
+  pinMode(openPin, OUTPUT);
+  pinMode(closePin, OUTPUT);
+  pinMode(stopPin, OUTPUT);
+  
+  while (!Serial) 
+  {
+  // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // set the data rate for the SoftwareSerial port
+  BTslave.begin(9600);
+}
+
+void loop() 
+{
+  if (BTslave.available()) 
+  {
+    Serial.write(BTslave.read());
+    moveDir = BTslave.read();
+    switch(moveDir)
+    {
+      case 'o':
+        digitalWrite(closePin, LOW);
+        digitalWrite(stopPin, LOW);
+        digitalWrite(openPin, HIGH);
+        Serial.println("o");
+        break;
+      case 'c':
+        digitalWrite(openPin, LOW);
+        digitalWrite(stopPin, LOW);
+        digitalWrite(closePin, HIGH);
+        break;
+      case 's':
+        digitalWrite(openPin, LOW);
+        digitalWrite(closePin, LOW);
+        digitalWrite(stopPin, HIGH);
+        break;
+      default:
+        digitalWrite(openPin, LOW);
+        digitalWrite(closePin, LOW);
+        digitalWrite(stopPin, HIGH);
+        break;
+    }
+  }
+}
+
